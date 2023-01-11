c52b42492eea899df9702454ab99d73112e6253d
smaleYolo <ilia.artamonov@yandex.ru>
2023-01-11 23:58:49 +0300

    gh pages (routing)

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,12 +9,14 @@ export const privateRoutes = [
     {path: '/about', element: About, exact: true},
     {path: '/404', element: NotFoundPage, exact: true},
     {path: '/posts', element: Posts, exact: true},
+    {path: '/react-fund-course', element: Posts, exact: true},
     {path: '/posts/:id', element: PostIdPage, exact: true},
     {path: '/', element: Posts, exact: true},
 ]
 
 export const publicRoutes = [
     {path: '/login', element: Login, exact: true},
+    {path: '/react-fund-course', element: Login, exact: true},
     {path: '/about', element: About, exact: true},
     {path: '/404', element: NotFoundPage, exact: true},
     {path: '/', element: Login, exact: true},
